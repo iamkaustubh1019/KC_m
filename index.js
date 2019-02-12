44f30ff66ee874f619f64e45dae946afff213e50
Kaustubh Chitravanshi <38200846+iamkaustubh1019@users.noreply.github.com>
2019-02-13 02:15:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/** @format */
+import {AppRegistry} from 'react-native';
+import Main from './src/Main';
+import {name as appName} from './app.json';
+global.Symbol = require('core-js/es6/symbol');
+require('core-js/fn/symbol/iterator');
+require('core-js/fn/map');
+require('core-js/fn/set');
+require('core-js/fn/array/find');
+
+AppRegistry.registerComponent(appName, () => Main);
