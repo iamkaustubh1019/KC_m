01dcfe9222491a29ac95f099cb1ce74872e56190
Kaustubh Chitravanshi <38200846+iamkaustubh1019@users.noreply.github.com>
2019-05-26 12:43:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,82 +1,80 @@
 import React, { Component } from 'react'
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Platform,
-  Image,
-  ActivityIndicator,YellowBox
-} from 'react-native'
+                StyleSheet,
+                Text,
+                View,
+                TouchableOpacity,
+                Platform,
+                Image,
+                ActivityIndicator,YellowBox
+        } from 'react-native'
 import ImagePicker from 'react-native-image-picker';
 import RNFetchBlob from 'react-native-fetch-blob';
 import firebase from 'firebase';
 import _ from 'lodash';
 
-YellowBox.ignoreWarnings(['Setting a timer']);
-const _console = _.clone(console);
-console.warn = message => {
-  if (message.indexOf('Setting a timer') <= -1) {
-    _console.warn(message);
-    Alert.alert(
-  'Alert Title',
-  'My Alert Msg',
-  [
-    {text: 'Try again'},
-    {text: 'OK', onPress: () => console.log('OK Pressed')},
-  ],
-  {cancelable: false},
-);
-  }
-};
+              YellowBox.ignoreWarnings(['Setting a timer']);
+              const _console = _.clone(console);
+              console.warn = message => {
+                if (message.indexOf('Setting a timer') <= -1) {
+                  _console.warn(message);
+                  Alert.alert(
+                'Alert Title',
+                'My Alert Msg',
+                [
+                  {text: 'Try again'},
+                  {text: 'OK', onPress: () => console.log('OK Pressed')},
+                ],
+                {cancelable: false},
+              );
+                }
+              };
 
 // Init Firebase
-var firebaseConfig = {
-   apiKey: "AIzaSyAhz126p5haYNL_VnZWLLiI7Ih-8JWcqU0",
-   authDomain: "mydatabase-593c3.firebaseapp.com",
-   databaseURL: "https://mydatabase-593c3.firebaseio.com",
-   projectId: "mydatabase-593c3",
-   storageBucket: "mydatabase-593c3.appspot.com",
-   messagingSenderId: "295649484262",
-   appId: "1:295649484262:web:883b23813a73c80c"
- };
- // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage()
+          var firebaseConfig = {
+              apiKey: "Something",
+              authDomain: "Your_Auth_Domain",
+              databaseURL: "Your_database_url",
+              projectId: "Your_project_id",
+              storageBucket: "Your_bucket",
+              messagingSenderId: "Your_id",
+              appId: "_App_ID_"
+            };
+            // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+          const storage = firebase.storage()
+          const Blob = RNFetchBlob.polyfill.Blob
+          const fs = RNFetchBlob.fs
+          window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+          window.Blob = Blob
 
-// Prepare Blob support
-const Blob = RNFetchBlob.polyfill.Blob
-const fs = RNFetchBlob.fs
-window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-window.Blob = Blob
+          const uploadImage = (uri, mime = 'application/octet-stream') => {
+            return new Promise((resolve, reject) => {
+              const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
+              const sessionId = new Date().getTime()
+              let uploadBlob = null
+              const imageRef = storage.ref('images').child(`${sessionId}`)
 
-const uploadImage = (uri, mime = 'application/octet-stream') => {
-  return new Promise((resolve, reject) => {
-    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
-    const sessionId = new Date().getTime()
-    let uploadBlob = null
-    const imageRef = storage.ref('images').child(`${sessionId}`)
-
-    fs.readFile(uploadUri, 'base64')
-      .then((data) => {
-        return Blob.build(data, { type: `${mime};BASE64` })
-      })
-      .then((blob) => {
-        uploadBlob = blob
-        return imageRef.put(blob, { contentType: mime })
-      })
-      .then(() => {
-        uploadBlob.close()
-        return imageRef.getDownloadURL()
-      })
-      .then((url) => {
-        resolve(url)
-      })
-      .catch((error) => {
-        reject(error)
-    })
-  })
-}
+              fs.readFile(uploadUri, 'base64')
+                .then((data) => {
+                  return Blob.build(data, { type: `${mime};BASE64` })
+                })
+                .then((blob) => {
+                  uploadBlob = blob
+                  return imageRef.put(blob, { contentType: mime })
+                })
+                .then(() => {
+                  uploadBlob.close()
+                  return imageRef.getDownloadURL()
+                })
+                .then((url) => {
+                  resolve(url)
+                })
+                .catch((error) => {
+                  reject(error)
+              })
+            })
+          }
 
 
 class Upload extends Component {
